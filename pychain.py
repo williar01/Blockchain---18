d9eefba14d40805c0f0b74801098b004a379e6d0
williar01 <90912414+williar01@users.noreply.github.com>
2022-04-09 13:59:32 -0400

    Add files via upload

--- pychain.py
+++ pychain.py
@@ -193,7 +193,8 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=input_data,
+        #data=input_data,
+        record =Record(sender, receiver, amount),
         creator_id=42,
         prev_hash=prev_block_hash
     )
